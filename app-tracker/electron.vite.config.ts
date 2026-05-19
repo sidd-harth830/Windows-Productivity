d16eccb651de7fae07441c08531b6b4d380f0ce0
Siddharth Yadav <leocarnivas@gmail.com>
2026-05-19 22:46:14 +0530

    completed dark theme UI and tailwind v4 setup

--- app-tracker/electron.vite.config.ts
+++ app-tracker/electron.vite.config.ts
@@ -1,16 +1,21 @@
 import { resolve } from 'path'
-import { defineConfig } from 'electron-vite'
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  main: {},
-  preload: {},
+  main: {
+    plugins: [externalizeDepsPlugin()]
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()]
+  },
   renderer: {
     resolve: {
       alias: {
         '@renderer': resolve('src/renderer/src')
       }
     },
-    plugins: [react()]
+    plugins: [react(), tailwindcss()]
   }
 })
